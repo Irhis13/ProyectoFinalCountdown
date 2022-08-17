a70f985f8636088480278e7d87b101d7bc015726
rverac <irhis13@gmail.com>
2022-08-17 18:29:55 +0200

    estado inicial github

---
+++ src/main/java/com/example/ProyectoFinalCountdownApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalCountdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalCountdownApplication.class, args);
+	}
+
+}
