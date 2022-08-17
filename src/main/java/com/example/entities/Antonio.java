a1e3128471e683b39bd61e41d33e4a2c094b9dc4
rverac <irhis13@gmail.com>
2022-08-17 19:12:23 +0200

    contraseña antonio

--- src/main/java/com/example/entities/Antonio.java
+++ src/main/java/com/example/entities/Antonio.java
@@ -5,4 +5,8 @@ public class Antonio {
     private String name;
     private String primerApellido;
     private String segundoApellido;
+
+    // token contraseña
+
+    // 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
 }
