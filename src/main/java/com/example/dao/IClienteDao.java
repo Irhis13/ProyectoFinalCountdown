c1f6161d6d950ee33c50ef12a7af5c4d609319c9
Aida-mm <aidamoyamontes@gmail.com>
2022-08-22 16:57:06 +0200

    Solucion Implementaciones

--- src/main/java/com/example/dao/IClienteDao.java
+++ src/main/java/com/example/dao/IClienteDao.java
@@ -3,7 +3,9 @@ package com.example.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.entities.Cliente;
+
 @Repository
-public interface IClienteDao extends JpaRepository {
+public interface IClienteDao extends JpaRepository<Cliente, Integer> {
     
 }
