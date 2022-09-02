c67a17d3a1e0bb2c66538e34a8e1aa360aa33f94
Asun <asunosa91@gmail.com>
2022-09-02 14:59:01 +0200

    Implementado fondo y menú

--- src/main/java/com/example/ProyectoFinalCountdownApplication.java
+++ src/main/java/com/example/ProyectoFinalCountdownApplication.java
@@ -49,39 +49,58 @@ public class ProyectoFinalCountdownApplication {
 			.build());
 
 			//Viajes
-			////////Categoria id:1
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 1").descripcion("BLA").precio(100.0)
-			.categoria(categoriaSrv.getCategoria(1)).build());
+			////////Categoria id:1 - AVENTURA
+			viajeSrv.guardar(Viaje.builder().nombre("Hazaña Inca").descripcion("Viaja al año 1460 y visita el mayor imperio de la Amércia precolombina. Contempla Machu Pichu en todo su esplendor y realiza el viaje de iniciación de los guerreros Incas, acompañado por uno de nuestros guías especializados .")
+			.precio(2500.0).categoria(categoriaSrv.getCategoria(1)).build());
 
+			viajeSrv.guardar(Viaje.builder().nombre("Travesía Vikinga").descripcion("Embárcate en el drakkar más famoso del rey vikingo Olaf Tryggvason en el año 998. Brinda en honor a Odin, el Vallhala te esperá.")
+			.precio(3250.0).categoria(categoriaSrv.getCategoria(1)).build());
+						
+			viajeSrv.guardar(Viaje.builder().nombre("Upside-Down").descripcion("Sólo para amantes de las emociones fuertes….. ¿Quieres conocer el mundo del Demogorgon y Vecna? Adiestra a tu propio demoperro y maravillate con el poder del Azotamentes.")
+			.precio(3250.0).categoria(categoriaSrv.getCategoria(1)).build());
+			
+			viajeSrv.guardar(Viaje.builder().nombre("Naboo").descripcion("Conoce las verdes llanuras de Naboo , sus colinas, frondosos bosques y bellas cascadas de agua cristalina y lagos o pantanos de densa profundidad donde los gungans habitan.")
+			.precio(2386.0).categoria(categoriaSrv.getCategoria(1)).build());
+			// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// //////////////////////////////////////////Categoria id:2 - CULTURAL
+			viajeSrv.guardar(Viaje.builder().nombre("Tierra Media").descripcion("Visita la Comarca año 2670 de la Tercera Edad, alójate en un acogedor agujero Hobbit, disfruta de los fuegos artificiales, goza de la gastronomía local y prueba la famosa hierba de la comarca.")
+			.precio(3250.0).categoria(categoriaSrv.getCategoria(2)).build());
+			
+			viajeSrv.guardar(Viaje.builder().nombre("Crucero Maravillas Mundo Antiguo").descripcion("Puedes visitar los grandes hitos de la cultura clásica que en un crucero, verás: el Faro de Alejandría, el Coloso de Rodas, el Templo de Artemisa, la Estatua de Zeus, el Mausoleo de Halicarnaso, y los Jardines Colgantes de Babilonia.")
+			.precio(3250.0).categoria(categoriaSrv.getCategoria(2)).build());
+			
+			viajeSrv.guardar(Viaje.builder().nombre("Orígenes del Jazz").descripcion("Si lo tuyo es la música... Años 20, Nueva Orleans, Lousiana…. El jazz comienza a despuntar como género propio, los músicos se reúnen en clubs clandestinos para improvisar. Únete a ellos y conoce de primera mano el orígen de estas melodías.")
+			.precio(3250.0).categoria(categoriaSrv.getCategoria(2)).build());
+
+			viajeSrv.guardar(Viaje.builder().nombre("Atlantis").descripcion("Descubre esta mítica cultura perdida descrita por Platón  como una isla situada más allá de las columnas de Hércules, más grande que Libia y Asia Menor juntas. Sumérgete en su cultura y maravillate de su avanzada tecnología.")
+			.precio(3250.0).categoria(categoriaSrv.getCategoria(2)).build());
+			// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// ////////Categoria id:3 - FAMILIAR
+			viajeSrv.guardar(Viaje.builder().nombre("Jurassic Park").descripcion("Viaja a  Isla Nublar y contempla el parque de ocio más famoso del mundo. Visita los laboratorios y aprende con Mr. ADN acerca del proceso de creación de nuestros dinosaurios. ")
+			.precio(2386.0).categoria(categoriaSrv.getCategoria(3)).build());
+			viajeSrv.guardar(Viaje.builder().nombre("Hogsmeade").descripcion("Especial Navidad en Hogsmeade. Disfruta de la cerveza de mantequilla, los helados de Florean Fortescue y de la tienda de artículos para bromas Zonko.")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(3)).build());
+			viajeSrv.guardar(Viaje.builder().nombre("Narnia").descripcion("Embárcate en el Lucero del Alba del Principe Caspian, prueba unas exquisitas delicias turcas en el palacio de la Reina de Hielo y disfruta de la compañía del Señor Tumnus o Aslan.")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(3)).build());
+			viajeSrv.guardar(Viaje.builder().nombre("Ruta de la Seda").descripcion("Contempla Samarkanda, la ciudad de los mil colores. Disfruta de las idas y venidas de mercaderes que les despachaban a romanos y persas las sedas de Oriente. Visita en familia los oasis repletos de animales exóticos que dieron fama a la leyenda.")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(3)).build());
+			
+			// ////////Categoria id:4
+			viajeSrv.guardar(Viaje.builder().nombre("París Bohemio").descripcion("Oh là là! París, año 1890. El barrio de Montmartre es el punto de reunión de los artistas más importantes del momento. La música, el teatro, los picnics en los canales….. ¿Acaso hay algo más romántico sobre la faz de la tierra? ")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(4)).build());
+			viajeSrv.guardar(Viaje.builder().nombre("Roma Años 60").descripcion("Disfruta la dolce vita como un auténtico italiano. Recorre la ciudad de Roma con tu Vespa junto a tu pareja, lanza una moneda en la Fontana di Trevi o simplemente dejate llevar por la magia en el ambiente.")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(4)).build());
+			viajeSrv.guardar(Viaje.builder().nombre("Mamma Mia").descripcion("Alójate en el Hotel Villa Donna y vive en primera persona el reenuentro de un amor de juventud en las islas griegas. Toda una experiencia rodeada por una música vibrante y llena de vida.")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(4)).build());
+			viajeSrv.guardar(Viaje.builder().nombre("Moulin Rouge").descripcion("En el París de Toulose-Lautrec, sé testigo de la hermosa y trágica historia de amor entre el bohemio Christian y la bella artsita Satine.  Porque como dice la canción: The greatest thing you'll ever learn is just to love and be loved in return…")
+			.precio(2125.0).categoria(categoriaSrv.getCategoria(4)).build());
+			
+
+			//VIAJES PARA AÑADIR EN LA LISTA
 			Viaje viajeCliente1 = viajeSrv.getViaje(1);
-			
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 1- Copia").descripcion("BLA").precio(100.0)
-			.categoria(categoriaSrv.getCategoria(1)).build());
-
 			Viaje viajeCliente2 = viajeSrv.getViaje(2);
-
-			////////Categoria id:2
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 2").descripcion("BLABLA").precio(200.0)
-			.categoria(categoriaSrv.getCategoria(2)).build());
-
 			Viaje viajeCliente3 = viajeSrv.getViaje(3);
-
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 2 - Copia").descripcion("BLABLA").precio(200.0)
-			.categoria(categoriaSrv.getCategoria(2)).build());
-			
 			Viaje viajeCliente4 = viajeSrv.getViaje(4);
-			////////Categoria id:3
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 3").descripcion("BLABLABLA").precio(300.0)
-			.categoria(categoriaSrv.getCategoria(3)).build());
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 3 - Copia").descripcion("BLABLABLA").precio(300.0)
-			.categoria(categoriaSrv.getCategoria(3)).build());
-
-			////////Categoria id:4
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 4").descripcion("BLABLABLABLA").precio(400.0)
-			.categoria(categoriaSrv.getCategoria(4)).build());
-			viajeSrv.guardar(Viaje.builder().nombre("Viaje 4 - Copia").descripcion("BLABLABLABLA").precio(400.0)
-			.categoria(categoriaSrv.getCategoria(4)).build());
-			
 			////////////LISTA VIAJES CLIENTE
 			List<Viaje> viajes1 = new ArrayList<>();
 			viajes1.add(viajeCliente1);
