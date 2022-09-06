f3b0ff1fba8afde36f1f6eeedf53dc6e84ff2017
Aida-mm <aidamoyamontes@gmail.com>
2022-09-06 19:20:42 +0200

    security aplicado

--- src/main/java/com/example/configurer/WebSecurityConfig.java
+++ src/main/java/com/example/configurer/WebSecurityConfig.java
@@ -3,14 +3,16 @@ package com.example.configurer;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     @Autowired
     private DataSource dataSource;
@@ -18,27 +20,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder passEncoder;
 
-    @Autowired
-    private LoginSuccessMessage successMessage;
+    // @Autowired
+    // private LoginSuccessMessage successMessage;
 
-  
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-          //Aquí van las urls que pueden ser accedidas sin restricciones
-        http.authorizeRequests().antMatchers("/home","/","/css/**","/js/**","/templates/**").permitAll()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/chronologic","/","/css/**","/js/**","/templates/**").permitAll()
         // Aquí las que si van a tener restricciones
-        .antMatchers("/formularioEmpleado/").hasAnyRole("ADMIN")
-        .antMatchers("/formularioCliente/").hasAnyRole("USER")
+        .antMatchers("/formularioEmpleado").hasAnyRole("ADMIN")
+        .antMatchers("/formularioCliente").hasAnyRole("USER")
         .anyRequest().authenticated()
         .and()
         .formLogin()
-            .successHandler(successMessage)
-            .loginPage("/login")
         .permitAll()
         .and()
         .logout().permitAll(); 
 
+        return http.build();
     }
+  
+    // @Bean
+    // public WebSecurityCustomizer webSecurityCustomizer() {
+    //     return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/css/**");
+    // }
+
 
     @Autowired
     public void configurerSecurityGlobal(AuthenticationManagerBuilder builder) throws Exception {
